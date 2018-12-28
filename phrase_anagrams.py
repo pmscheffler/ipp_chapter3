72df9c77c44c1532254bfb1a7fe577b87a62b226
pmscheffler <scheff@f5.com>
2018-12-27 23:40:54 -0500

    Done part 3

--- phrase_anagrams.py
+++ phrase_anagrams.py
@@ -50,4 +50,39 @@ def process_choice(name):
     name = ''.join(left_over_list)  # make display more readable
     return choice, name
 
+def main():
+    """Help user build anagram phrase from their name."""
+    name = .join(ini_name.lower().split())
+    name = name.replace('-', '')
+    limit = len(name)
+    phrase = ''
+    running = True
+
+    while running:
+        temp_phrase = phrase.replace(' ', '')
+        if len(temp_phrase) < limit:
+            print("Length of anagram phrase = {}".format(len(temp_phrase)))
+
+            find_anagrams(name, dict_file)
+            print("Current anagram phrase =", end=" ")
+            print(phrase, file=sys.stderr)
+
+            choice, name = process_choice(name)
+            phrase += choice + ' '
+
+        elif len(temp_phrase) == limit:
+            print("\n***** FINISHED **** \n")
+            print("Anagram of name =", end=" ")
+            print(phrase, file=sys.stderr)
+            print()
+            try_again = input('\n\nTry again? (Press Enter else "n" to quit\n ')
+            if try_again.lower() == 'n':
+                running = False
+                sys.exit
+            else:
+                main()
+
+if __name__ == '__main__':
+    main()
+
     
