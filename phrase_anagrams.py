8c6c1bfbc6f5e7267bcf28f242f6ba8850d16b84
Peter Scheffler <p.scheffler@f5.com>
2018-12-27 23:08:27 -0500

    Done phase 1

--- phrase_anagrams.py
+++ phrase_anagrams.py
@@ -2,3 +2,29 @@ import sys
 from collections import Counter
 import load_dictionary
 
+dict_file = load_dictionary.load('2of4brif.txt')
+# ensure "a" and "I" (both lowercase) are included
+dict_file.append('a')
+dict_file.append('i')
+dict_file = sorted(dict_file)
+
+ini_name = input("Enter a name: ")
+
+def find_anagrams(name, word_list):
+    """ Read name & dictionary file & display all anagrams IN name."""
+    name_letter_map = Counter(name)
+    anagrams = []
+    for word in word_list:
+        test = ''
+        word_letter_map = Counter(word.lower())
+        for letter in word:
+            if word_letter_map[letter] <= name_letter_map[letter]:
+                test += letter
+            if Counter(test) == word_letter_map:
+                anagrams.append(word)
+    print(*anagrams, sep='\n')
+    print()
+    print("Remaining letters = {}".format(name))
+    print("Number of remaining letters = {}".format(len(name)))
+    print("Number of remaining (real world) anagrams = {}".format(len(anagrams)))
+    
